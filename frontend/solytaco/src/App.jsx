b4b6c3428198dcfef39819d5855e86dd06524340
Victor <victorm.martinezjr@outlook.com>
2025-10-24 22:13:56 -0400

    initialize project

---
+++ frontend/solytaco/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Home />
+    </main>
+  );
+}
+
+export default App;
